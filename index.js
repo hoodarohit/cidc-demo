0ec52afb0dead30bb0eb5afb0c47a6e4d9ebedda
Ubuntu <ubuntu@ip-172-31-12-89.ap-south-1.compute.internal>
2026-01-18 12:25:56 +0000

    Initial commit: basic Node.js app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello CI/CD 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`App running on port ${PORT}`);
+});
